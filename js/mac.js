bc1c76841c57f4fb8869b62c4f67a3466d9dcb56
saifulislammunna <saifulmunna28@gmail.com>
2021-08-21 19:46:13 +0600

    js added

--- js/mac.js
+++ js/mac.js
@@ -1,34 +1,64 @@
- function updateExtraCost( product,price){
-    const extraMemoryCost = document.getElementById(product + '-cost');
-    extraMemoryCost.innerText = price;
+ // function for different configuration cost
+ function updateProductCost( product,price){
+    const productExtraCost = document.getElementById(product + '-cost');
+    productExtraCost.innerText = price;
+    // calling calculate total
+    calculateTotal();
 }
+ 
+ 
+ // function for calculate total 
+
+function calculateTotal(){
+    const bestPriceCost = 1299;
+    const extraMemoryCost = parseInt( document.getElementById('extra-memory-cost').innerText);
+    const extraStorageCost = parseInt( document.getElementById('extra-storage-cost').innerText);
+    const deliveryChargeCost = parseInt( document.getElementById('delivery-charge-cost').innerText);
+     
+    // calculating total price
+    const totalPrice = bestPriceCost + extraMemoryCost + extraStorageCost + deliveryChargeCost;  
+    //  update on html
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
    
 
-
+// handling memory extra cost with event-handler 
 document.getElementById('memory-8gb').addEventListener('click',function( ){
-    updateExtraCost('extra-memory',0);
+    updateProductCost('extra-memory',0);
+     
+     
 });
 
 document.getElementById('memory-16gb').addEventListener('click',function(){
-    updateExtraCost('extra-memory',180);
+    updateProductCost('extra-memory',180);
+     
+    
 });
-
+// handling storage extra cost with event-handler 
 document.getElementById('storage-256gb').addEventListener('click',function(){
-    updateExtraCost('extra-storage',0);
+    updateProductCost('extra-storage',0);
+   
 });
 
 document.getElementById('storage-512gb').addEventListener('click',function(){
-    updateExtraCost('extra-storage',100);
+    updateProductCost('extra-storage',100);
+     
 });
 
 document.getElementById('storage-1tb').addEventListener('click',function(){
-    updateExtraCost('extra-storage', 180);
+    updateProductCost('extra-storage', 180);
+     
 });
-
+// handling delivery extra cost with event-handler 
 document.getElementById('delay-delivery').addEventListener('click',function(){
-    updateExtraCost('delivery-charge',0);
+    updateProductCost('delivery-charge',0);
+     
 });
 
 document.getElementById('fast-delivery').addEventListener('click',function(){
-    updateExtraCost('delivery-charge',20);
+    updateProductCost('delivery-charge',20);
+     
 });
+
+ 
