140a4c68ae30869a0d6ed6be0a03e417609730b9
saifulislammunna <saifulmunna28@gmail.com>
2021-08-21 11:07:27 +0600

    js added

--- js/mac.js
+++ js/mac.js
@@ -1,3 +1,30 @@
+// function 
+
+
 document.getElementById('memory-8gb').addEventListener('click',function(){
+    console.log('clicked');
+});
+
+document.getElementById('memory-16gb').addEventListener('click',function(){
+    console.log('clicked');
+})
+
+document.getElementById('storage-256gb').addEventListener('click',function(){
+    console.log('clicked');
+});
+
+document.getElementById('storage-512gb').addEventListener('click',function(){
+    console.log('clicked');
+})
+
+document.getElementById('storage-1tb').addEventListener('click',function(){
+    console.log('clicked');
+});
+
+document.getElementById('delay-delivery').addEventListener('click',function(){
+    console.log('clicked');
+})
+
+document.getElementById('fast-delivery').addEventListener('click',function(){
     console.log('clicked');
 })
