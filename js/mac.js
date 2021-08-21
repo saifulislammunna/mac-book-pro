7bd42758b25556ea9f40be8848e80b1e347baedb
saifulislammunna <saifulmunna28@gmail.com>
2021-08-21 13:46:25 +0600

    js added

--- js/mac.js
+++ js/mac.js
@@ -1,30 +1,34 @@
-// function 
+ function updateExtraCost( product,price){
+    const extraMemoryCost = document.getElementById(product + '-cost');
+    extraMemoryCost.innerText = price;
+}
+   
 
 
-document.getElementById('memory-8gb').addEventListener('click',function(){
-    console.log('clicked');
+document.getElementById('memory-8gb').addEventListener('click',function( ){
+    updateExtraCost('extra-memory',0);
 });
 
 document.getElementById('memory-16gb').addEventListener('click',function(){
-    console.log('clicked');
-})
+    updateExtraCost('extra-memory',180);
+});
 
 document.getElementById('storage-256gb').addEventListener('click',function(){
-    console.log('clicked');
+    updateExtraCost('extra-storage',0);
 });
 
 document.getElementById('storage-512gb').addEventListener('click',function(){
-    console.log('clicked');
-})
+    updateExtraCost('extra-storage',100);
+});
 
 document.getElementById('storage-1tb').addEventListener('click',function(){
-    console.log('clicked');
+    updateExtraCost('extra-storage', 180);
 });
 
 document.getElementById('delay-delivery').addEventListener('click',function(){
-    console.log('clicked');
-})
+    updateExtraCost('delivery-charge',0);
+});
 
 document.getElementById('fast-delivery').addEventListener('click',function(){
-    console.log('clicked');
-})
+    updateExtraCost('delivery-charge',20);
+});
