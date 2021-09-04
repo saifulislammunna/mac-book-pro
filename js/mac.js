68171c7d33326756c96c5aa37c20a207bef70614
saifulislammunna <saifulmunna28@gmail.com>
2021-09-04 13:32:03 +0600

    pin added

--- js/mac.js
+++ js/mac.js
@@ -1,80 +1,85 @@
- // function for different configuration cost
- function updateProductCost( product,price){
+// function for different configuration cost
+function updateProductCost(product, price) {
     const productExtraCost = document.getElementById(product + '-cost');
     productExtraCost.innerText = price;
     // calling calculate total
     calculateTotal();
+    
 }
 
- // function for calculate total 
+// function for calculate total 
 
-function calculateTotal(){
+function calculateTotal() {
     const bestPriceCost = 1299;
-    const extraMemoryCost = parseInt( document.getElementById('extra-memory-cost').innerText);
-    const extraStorageCost = parseInt( document.getElementById('extra-storage-cost').innerText);
-    const deliveryChargeCost = parseInt( document.getElementById('delivery-charge-cost').innerText);
-     
+    const extraMemoryCost = parseInt(document.getElementById('extra-memory-cost').innerText);
+    const extraStorageCost = parseInt(document.getElementById('extra-storage-cost').innerText);
+    const deliveryChargeCost = parseInt(document.getElementById('delivery-charge-cost').innerText);
+
     // calculating total price
-    const totalPrice = bestPriceCost + extraMemoryCost + extraStorageCost + deliveryChargeCost;  
+    const totalPrice = bestPriceCost + extraMemoryCost + extraStorageCost + deliveryChargeCost;
     //  update on html
     document.getElementById('total-price').innerText = totalPrice;
     //  bonus part
     document.getElementById('total-price-bottom').innerText = totalPrice;
+    
 }
 
 // bonus part
 
-function verifyPin(){
-    let total = document.getElementById('total-price-bottom').innerText;
-     
-    const pin =  'stevekaku';
-    const typedPin = document.getElementById('typed-pin').innerText;
-    if(pin.innerText != typedPin){
-    const changedTotal = total - total/5 ;
-     // update on html 
-     document.getElementById('total-price-bottom').innerText = changedTotal;
-    }  
-   
-}    
+function verifyPin() {
+    let total = document.getElementById('total-price').innerText ;
+    const pin = 'stevekaku';
+    const typedPin = document.getElementById('typed-pin').value;
+
+    if (typedPin == pin ) {
+
+
+        const changedTotal = total - total / 5;
+        // update on html 
+
+        // const text = total.innerText;
+        // console.log(text);
+        
+        document.getElementById('total-price-bottom').innerText = changedTotal;
+    }
+
+}
+// verifyPin();
 
 // handling memory extra cost with event-handler 
-document.getElementById('memory-8gb').addEventListener('click',function( ){
-    updateProductCost('extra-memory',0);
-     
-     
+document.getElementById('memory-8gb').addEventListener('click', function () {
+    updateProductCost('extra-memory', 0);
+
+
 });
 
-document.getElementById('memory-16gb').addEventListener('click',function(){
-    updateProductCost('extra-memory',180);
-     
-    
+document.getElementById('memory-16gb').addEventListener('click', function () {
+    updateProductCost('extra-memory', 180);
+
+
 });
 // handling storage extra cost with event-handler 
-document.getElementById('storage-256gb').addEventListener('click',function(){
-    updateProductCost('extra-storage',0);
-   
+document.getElementById('storage-256gb').addEventListener('click', function () {
+    updateProductCost('extra-storage', 0);
+
 });
 
-document.getElementById('storage-512gb').addEventListener('click',function(){
-    updateProductCost('extra-storage',100);
-     
+document.getElementById('storage-512gb').addEventListener('click', function () {
+    updateProductCost('extra-storage', 100);
+
 });
 
-document.getElementById('storage-1tb').addEventListener('click',function(){
+document.getElementById('storage-1tb').addEventListener('click', function () {
     updateProductCost('extra-storage', 180);
-     
+
 });
 // handling delivery extra cost with event-handler 
-document.getElementById('delay-delivery').addEventListener('click',function(){
-    updateProductCost('delivery-charge',0);
-     
+document.getElementById('delay-delivery').addEventListener('click', function () {
+    updateProductCost('delivery-charge', 0);
+
 });
 
-document.getElementById('fast-delivery').addEventListener('click',function(){
-    updateProductCost('delivery-charge',20);
-     
+document.getElementById('fast-delivery').addEventListener('click', function () {
+    updateProductCost('delivery-charge', 20);
+
 });
-
- 
-
- 
